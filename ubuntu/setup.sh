8dc9a6cb545d16601105f3373bb7bf84d32d2b1f
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-11 00:16:17 -0700

    update Ubuntu's setup.sh to make it able to install OpenJDK;

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,1 +1,6 @@
 #! /bin/bash
+echo '==============='
+echo 'Setup starts...'
+echo '<<< Installing OpenJDK 7...'
+sudo apt-get install openjdk-7-jdk
+echo '>>> OpenJDK 7 has been successfully installed!'
