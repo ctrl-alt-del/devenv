33ed104876d2823e9e56db78982a69af62b88b42
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-14 12:11:59 -0700

    update Ubuntu's setup.sh to make it able to install OpenSSL;

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 echo '==============='
 echo 'Setup starts...'
+echo '<<< Installing/Upgrading OpenSSL...'
+sudo apt-get upgrade openssl
+echo '>>> OpenSSL has been successfully installed/upgraded!'
+
 echo '<<< Installing OpenJDK 7...'
 sudo apt-get install openjdk-7-jdk
 echo '>>> OpenJDK 7 has been successfully installed!'
