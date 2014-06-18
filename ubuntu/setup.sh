6a95acd544ff09fa502b3441832a08c2641d0b73
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-17 19:09:17 -0700

    update Ubuntu's setup.sh to make it able to install Tilda;

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 echo '==============='
 echo 'Setup starts...'
+echo '<<< Installing Tilda...'
+sudo apt-get install tilda
+echo '>>> Tilda has been successfully installed!'
+
 echo '<<< Installing/Upgrading Git...'
 sudo apt-get upgrade git
 echo '>>> Git has been successfully installed/upgraded!'
