ba2b325fa7b148efb71cf4b8bd8902c50c40286a
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-28 15:17:00 -0700

    update setup.sh for installing Homebrew;

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,1 +1,6 @@
 #! /bin/bash
+echo '==============='
+echo 'Setup starts...'
+echo '<<< Installing Homebrew...'
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+echo '>>> Homebrew has been successfully installed!'
