a36311341be9de12a774c226610fec3a68dc2d06
ctrl-alt-del <jiyang881230@gmail.com>
2014-06-29 10:53:38 -0700

    update setup.sh for installing Git;

--- mac/setup.sh
+++ mac/setup.sh
@@ -4,3 +4,8 @@ echo 'Setup starts...'
 echo '<<< Installing Homebrew...'
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 echo '>>> Homebrew has been successfully installed!'
+
+echo '<<< Upgrading Git...'
+git clone https://github.com/git/git
+echo '>>> Git has been successfully upgraded!'
+
