6c047970a7c5339758b2e4b57d2ca7a4a9052bb9
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-02 19:41:32 -0700

    update setup.sh for setting up .bash_profile on Windows;

--- windows/setup.sh
+++ windows/setup.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+# execture the setup_bash_profile file to generate ~/.bash_profile
+echo '==============='
+echo 'Setup starts...'
+echo '<<< Setting up ~/.bash_profile...'
+local file="setup_bash_profile"
+if [ -f "$file" ] ; then
+    ./setup_bash_profile
+    echo '>>> ~/.bash_profile has been successfully created!'
+fi
+
