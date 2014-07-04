0f03755be4c61129fbd4d79fb446bb4a76ee94b2
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-03 20:31:30 -0700

    update Mac's setup.sh to make it able to create .bash_profile;

--- mac/setup.sh
+++ mac/setup.sh
@@ -9,3 +9,9 @@ echo '<<< Upgrading Git...'
 git clone https://github.com/git/git
 echo '>>> Git has been successfully upgraded!'
 
+echo '<<< Setting up ~/.bash_profile...'
+local file="setup_bash_profile"
+if [ -f "$file" ] ; then
+    bash ../setup_bash_profile.sh
+    echo '>>> ~/.bash_profile has been successfully created!'
+fi
