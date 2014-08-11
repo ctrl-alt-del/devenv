3d73f150a42f9a403e34be205576ba9473618766
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-11 00:11:59 -0700

    update fedora/setup.sh;

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -1,1 +1,55 @@
 #! /bin/bash
+echo '==============='
+echo 'Setup starts...'
+echo '<<< Installing Guake...'
+sudo yum install guake
+echo '>>> Guake has been successfully installed!'
+echo ' '
+
+echo '<<< Installing Zsh...'
+sudo yum install zsh
+echo '>>> Zsh has been successfully installed!'
+echo ' '
+
+# install oh-my-zsh
+# config oh-my-zsh chsh -s 'which zsh'
+
+echo '<<< Installing/Upgrading Git...'
+sudo yum install git-core
+echo '>>> Git has been successfully installed/upgraded!'
+echo ' '
+
+echo '<<< Installing Gitk...'
+sudo yum install gitk
+echo '>>> Gitk has been successfully installed!'
+
+echo '<<< Installing OpenJDK 7...'
+sudo yum install openjdk-7-jdk
+echo '>>> OpenJDK 7 has been successfully installed!'
+
+echo '<<< Installing Node.js...'
+sudo yum install nodejs nodejs-dev npm
+echo '>>> Node.js has been successfully installed!'
+
+echo '<<< Installing Curl...'
+sudo yum install curl
+echo '>>> Curl has been successfully installed!'
+
+echo '<<< Setting up ~/.bash_profile...'
+if [ ! -f ./setup_bash_profile.sh ] ; then
+  \curl -O https://raw.githubusercontent.com/ctrl-alt-del/devenv/master/util/setup_bash_profile.sh
+fi
+chmod 755 setup_bash_profile.sh
+bash ./setup_bash_profile.sh
+rm setup_bash_profile.sh
+
+echo '>>> ~/.bash_profile has been successfully created!'
+
+echo '<<< Installing RVM, Ruby, and Rails...'
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+echo '>>> RVM, Ruby, and Rails have been successfully installed!'
+
+echo '<<< Configuring Ruby environment...'
+rvm requirements
+echo '>>> Ruby environment has been successfully configured!'
+
