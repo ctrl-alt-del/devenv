665a9ce0eb50495254d6051f9b546b10fba2ee78
ctrl-alt-del <jiyang881230@gmail.com>
2014-10-07 22:24:54 -0700

    update util/setup_synclient.sh;

--- util/setup_synclient.sh
+++ util/setup_synclient.sh
@@ -9,10 +9,10 @@ write_to_file() {
   synclient TapAndDragGesture=0
   synclient TapButton1=1
   synclient TapButton2=3
-  synclient TapButton3=0
-  synclient FingerHigh=50
-  synclient VertScrollDelta=-400
-  synclient HorizScrollDelta=-400
+  synclient TapButton3=2
+  synclient FingerHigh=45
+  synclient VertScrollDelta=-350
+  synclient HorizScrollDelta=-350
   synclient RTCornerButton=0
   synclient RBCornerButton=0
   synclient LTCornerButton=0
@@ -20,7 +20,6 @@ write_to_file() {
   synclient PalmDetect=1
   synclient MinSpeed=0.7
   synclient MaxSpeed=1.7
-  synclient PalmDetect=1
   ";
 
   echo "$content" >> ~/.profile;
