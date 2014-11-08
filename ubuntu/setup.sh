ac1241d0f29b16d0ee51cbeb033468c1521bc2bb
ctrl-alt-del <jiyang881230@gmail.com>
2014-11-07 21:19:59 -0800

    update ubuntu/setup.sh with script of installing sublime;

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -28,6 +28,7 @@ echo ' '
 echo '<<< Installing Sublime...'
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo apt-get update
+sudo apt-get install sublime-text
 echo '>>> Sublime has been successfully installed!'
 
 echo '<<< Installing Gitk...'
