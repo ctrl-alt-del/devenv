d728100ccded5ba192a3a80119c610fca32b8a10
ctrl-alt-del <jiyang881230@gmail.com>
2014-11-08 00:14:47 -0800

    update util/setup_synclient.sh;

--- util/setup_synclient.sh
+++ util/setup_synclient.sh
@@ -5,25 +5,28 @@ write_to_file() {
     touch ~/.profile
   fi
 
-  content="
-  synclient TapAndDragGesture=0
-  synclient TapButton1=1
-  synclient TapButton2=3
-  synclient TapButton3=2
-  synclient FingerHigh=45
-  synclient VertScrollDelta=-350
-  synclient HorizScrollDelta=-350
-  synclient RTCornerButton=0
-  synclient RBCornerButton=0
-  synclient LTCornerButton=0
-  synclient LBCornerButton=0
-  synclient PalmDetect=1
-  synclient MinSpeed=0.7
-  synclient MaxSpeed=1.7
-  ";
-
-  echo "$content" >> ~/.profile;
+  echo "$1" >> ~/.profile;
 }
 
+content="
+synclient TapAndDragGesture=0
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=2
+synclient FingerHigh=45
+synclient VertScrollDelta=-350
+synclient HorizScrollDelta=-350
+synclient RTCornerButton=0
+synclient RBCornerButton=0
+synclient LTCornerButton=0
+synclient LBCornerButton=0
+synclient PalmDetect=1
+synclient MinSpeed=0.7
+synclient MaxSpeed=1.7
+
+source \$HOME/.rvm/scripts/rvm
+
+";
+
 # execute it
-write_to_file
+write_to_file "$content"
