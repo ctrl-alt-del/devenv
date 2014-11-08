6e2246df7b987a294fe81c20ddd3e880b33aeba2
ctrl-alt-del <jiyang881230@gmail.com>
2014-11-07 22:32:32 -0800

    update util/setup_bashrc.sh;

--- util/setup_bashrc.sh
+++ util/setup_bashrc.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+write_to_file() {
+
+	if [ ! -f ~/.bashrc ] ; then
+		touch ~/.bashrc
+	fi
+
+	echo "$1" >> ~/.bashrc;
+}
+
+content="
+source \$HOME/.rvm/scripts/rvm
+";
+
+# execute it
+write_to_file "$content"
